ee808e16d6cdac0523c2db3e19e8bc5f04b14c42
Kat <katkatvdd@yahoo.com.ph>
2022-06-04 17:11:50 +0800

    Modified 1 project file

--- src/projects/files.js
+++ src/projects/files.js
@@ -6,17 +6,17 @@ import MemorySmall from './images/Memory-small.PNG';
 import MemoryLarge from './images/Memory-large.PNG';
 import TodoSmall from './images/Todo-small.PNG';
 import TodoLarge from './images/Todo-large.PNG';
-import CVSmall from './images/CV-small.PNG';
-import CVLarge from './images/CV-large.PNG';
+import LibrarySmall from './images/Library-small.PNG';
+import LibraryLarge from './images/Library-large.PNG';
 import TicTacToeSmall from './images/TicTacToe-small.PNG';
 import TicTacToeLarge from './images/TicTacToe-large.PNG';
 
 const projects = [
     {id: 1, name: "Best Buys: Shopping Cart", small: ShoppingCartSmall, large: ShoppingCartLarge, description:  "A basic shopping cart app built with React's react-router-dom to display multiple pages", repoLink: "https://github.com/LetsGo12300/shopping-cart", demoLink: "https://letsgo12300.github.io/shopping-cart/"},
     {id: 2, name: "Memory Game", small: MemorySmall, large: MemoryLarge, description:  "A memory game built with React featuring Asian flags", repoLink: "https://github.com/LetsGo12300/memory-game", demoLink: "https://letsgo12300.github.io/memory-game/"},
-    {id: 3, name: "CV Application", small: CVSmall, large: CVLarge, description: "Create your own CV application using React", repoLink: "https://github.com/LetsGo12300/cv-project", demoLink: "#"},
+    {id: 3, name: "Library App", small: LibrarySmall, large: LibraryLarge, description: "A simple book tracking web app created in HTML, CSS, and JavaScript. Users can create, update, delete books in the app.", repoLink: "https://github.com/LetsGo12300/library", demoLink: "https://letsgo12300.github.io/library/"},
     {id: 4, name: "To Do List App", small: TodoSmall, large: TodoLarge, description: "A web-based to-do list application created with JavaScript, HTML, and CSS. Webpack was used as the bundler of Javascript modules.", repoLink: "https://github.com/LetsGo12300/to-do-list", demoLink: "https://letsgo12300.github.io/to-do-list/"},
-    {id: 5, name: "Restaurant Page", small: RestaurantSmall, large: RestaurantLarge, description: "A restaurant page project I designed using JavaScript, built with Webpack, featuring Asian-inspired dishes", repoLink: "https://github.com/LetsGo12300/restaurant-page", demoLink: "https://letsgo12300.github.io/restaurant-page/"},
+    {id: 5, name: "Restaurant Page", small: RestaurantSmall, large: RestaurantLarge, description: "A restaurant page project designed using JavaScript, built with Webpack, featuring Asian-inspired dishes", repoLink: "https://github.com/LetsGo12300/restaurant-page", demoLink: "https://letsgo12300.github.io/restaurant-page/"},
     {id: 6, name: "Tic-Tac-Toe", small: TicTacToeSmall, large: TicTacToeLarge, description: "Tic-Tac-Toe game built using JavaScript factory functions and module patterns", repoLink: "https://github.com/LetsGo12300/tic-tac-toe", demoLink: "https://letsgo12300.github.io/tic-tac-toe/"}
 ];
 
